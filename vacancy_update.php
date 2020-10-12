14b9ba90c4a00b649fe8847350482bd19fb49c24
RushdyAhamed <rushdhyahamed98@gmail.com>
2020-10-12 08:46:45 +0530

    form_changed

--- vacancy_update.php
+++ vacancy_update.php
@@ -25,60 +25,117 @@
 
         <!-- Bootstrap CSS -->
      <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" /> -->
-        <link rel='stylesheet' type='text/css' href='css/vacancy.css'/>
+     <!--   <link rel='stylesheet' type='text/css' href='css/vacancy.css'/>     -->
         <link rel='stylesheet' type='text/css' href='css/bootstrap.min.css'/>
         <script src="js/jquery-3.5.1.min.js"></script>
 
-      <!--  <link type='text' href='inc/config_vacancy.php'/>  -->
-    </head>
+        <!-- Added for validation part  -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"></link>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.min.js"></script>
+
+        <script type='text/javascript' src="js/back_2top.js"></script>    
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+<style>
+
+/*Include validation css */
+
+/* body {
+            background: #EECDA3;
+            background: -webkit-linear-gradient(to top, #EF629F, #EECDA3);
+            background: linear-gradient(to top, #EF629F, #EECDA3);
+        }
+*/
+.container {
+            max-width: 550px;
+        }
+
+        .has-error label,
+        .has-error input,
+        .has-error textarea {
+            color: red;
+            border-color: red;
+        }
+
+        .list-unstyled li {
+            font-size: 13px;
+            padding: 4px 0 0;
+            color: red;
+        }
+
+
+/*Back to top css*/
+
+.back-to-top {
+    position: fixed;
+    bottom: 25px;
+    right: 25px;
+    display: none;
+}
+</style>
+</head>
 <body style="background-color:;">
 <?php include('header.php'); ?>
 
 <!-- $date = DateTime::createFromFormat('d/m/Y', date);  -->
-
-<form action="config_vacancy.php" method="post" enctype="multipart/form-data">
+<div class="container mt-5">
+        <div class="card" style="border: 1px solid #4115bb">
+            <div class="card-body">
+                <form action="config_vacancy.php" method="post" enctype="multipart/form-data" role="form" data-toggle="validator">
   
 
  <!-- <div class="container">       -->
 
- <div class="input-group">
+ <div class="form-group">
 <input type="hidden" name="Id" value="<?php echo $Id; ?>">
 </div>
 
- <div class="input-group">   
+<div class="form-group">
     <label for="position">Position</label>
-    <input type="text" name="Position" value="<?php echo $Position; ?>">
+    <input type="text" name="Position" class="form-control" id="inputPosition" maxlength="80" minlength="5" pattern="^[a-z A-Z_.-]*$" name="Position" value="<?php echo $Position; ?>" required>
    <!-- <input type="text" placeholder="Enter position" name="Position" required>  -->
+
+    <!-- Error -->
+    <div class="help-block with-errors"></div>
 </div>
 
-<div class="input-group">
+<div class="form-group">
     <label for="opndate">Opening Date</label>
-    <input type="date" name="Opendate" readonly="readonly" value="<?php echo $Opendate; ?>">
+    <input type="date" class="form-control" name="Opendate" readonly="readonly" value="<?php echo $Opendate; ?>">
 
    <!-- <input type="date" value="<?php echo date('Y-m-d'); ?>" >  -->
 
    <!-- <input type="date" placeholder="Open date - YYYY/MM/DD" name="date" > -->
 </div>
 
-<div class="input-group">
+<div class="form-group">
     <label for="clsdate">Closing Date</label>
-    <input type="date" name="Closedate" value="<?php echo $Closedate; ?>">
+    <input type="date" class="form-control" id="inputDate" name="Closedate" value="<?php echo $Closedate; ?>" required>
+
+    <!-- Error -->
+    <div class="help-block with-errors"></div>
 </div>
 
-<div class="input-group">
-    <label for="img">Select image to upload</label>
-     <input type="file" name="Advertisement" accept="image/x-png,image/jpeg" />
+<div class="form-group"> 
+   <label for="img">Advertisement</label>
+   <input type="file" name="Advertisement" class="form-control" Id="Advertisement" accept="image/x-png,image/jpeg">
    <!-- <input type="file" name="fileToUpload" id="fileToUpload" >  -->
    <!-- <input type="submit" value="Upload Image" name="submit">    -->
-    </form>
+    
 
-    <div class="input-group">
-    <button class="btn btn-success" type="submit" name="update">Update</button>
-    </div>
 </div>
+
+<div class="form-group">
+    <button class="btn btn-success btn-block" type="submit" name="update">Update</button>
 </div>
 
 </form>
+         </div>
+    </div>
+</div>
+<!--back to top -->
+<a id="back-to-top" href="#" class="btn btn-dark btn-lg back-to-top" style='background-color:royalblue' role="button"><i class="fas fa-chevron-up"></i></a>
+  
 <?php include('footer1.php'); ?>
 
 </body>
