ff6e0d4558e65df8e4a1348977bee88db8ca2d49
geethanchanaka <chanakageethan@gmail.com>
2020-10-15 20:50:41 +0530

    Login regiter added to navbar

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!--    <script src="js/jquery-3.5.1.min.js"></script> -->
 
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel = "stylesheet" href="css/navbar.css">
 
 
@@ -29,6 +31,9 @@
   .sticky + .content {
     padding-top: 60px;
   }
+
+
+ 
   
 </style>
 </head>
@@ -48,12 +53,7 @@
 <div class="col-sm-2 ">
 
 
-<div class="Login_Button_Div">
-    <button type="button"  class="btn btn-primary float-right"> Login</button>
-    <button type="button"  class="btn btn-primary float-right" > SignUp</button>
 
-
-</div>
 
 </div>
 
@@ -78,7 +78,20 @@
     <a href="#">Promotions</a>
     <a href="headhunter.php">Headhunter Team</a>
     <a href="contact_us.php">Contact</a>
+   
+    <a href="#"  style="float: right;"><i class="fa fa-fw fa-user"></i> Login</a>
+    <a href="#"  style="float: right;"><i class="fa fa-fw fa-user"></i> Register</a>
+ 
     <a href="javascript:void(0);" style="font-size:14px;" class="icon" onclick="myFunction()">&#9776;</a>
+
+
+
+</ul>
+
+
+
+
+
     </div>
  </div> 
 
