f6a19c9703f1c7c5b78df0805ae15b02501c191a
Samare97 <maxwiduranga@gmail.com>
2020-10-15 10:05:31 +0530

    add email

--- add_feedback.php
+++ add_feedback.php
@@ -10,39 +10,49 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="css/contact.css" />
+    <link rel="stylesheet" href="css/Feedback_CSS/add_feedback.css" />
 </head>
 <body>
-    <div class="panel-container">
+    <div id="panel" class="panel-container">
     <strong>GIVE US FEEDBACK</strong>
+    <br>
+    <div >
+    <textarea class="email" type="textarea" name="email" id="email" placeholder="Your Email" maxlength="500" rows="1"></textarea>
+    </div>
         <div class="rating-container">
             <div class="rating">
                 <img class="angry" src="image/Feedback_images/Angry.png" alt=""/>
-                <small>Very Bad</small>
+                <br><small>Worst</small>
             </div>
 
             <div class="rating">
                 <img class="sad" src="image/Feedback_images/Sad.png" alt=""/>
-                <small>Bad</small>
+                <br><small>Bad</small>
             </div>
 
             <div class="rating">
                 <img class="neutral" src="image/Feedback_images/Neutral.png" alt=""/>
-                <small>Neutral</small>
+                <br><small>Neutral</small>
             </div>
 
             <div class="rating">
                 <img class="happy" src="image/Feedback_images/Happy.png" alt=""/>
-                <small>Good</small>
+                <br><small>Good</small>
             </div>
 
             <div class="rating">
                 <img class="love" src="image/Feedback_images/Love.png" alt=""/>
-                <small>Very Good</small>
+                <br><small>excellent</small>
             </div>
 
-
+        
         </div>
+
+            <div>
+           
+            <textarea class="form-control" type="textarea" name="comments" id="comments" placeholder="Your Comments" maxlength="6000" rows="7"></textarea>
+            </div>
+            <br>
         <button class="btn" id="send">SEND</button>
 
     </div>
