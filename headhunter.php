c7472903fe77aa10724e9fc78f512108a6b61b07
Samare97 <maxwiduranga@gmail.com>
2020-10-15 13:18:49 +0530

    add headhunter

---
+++ headhunter.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/Headhunter_CSS/headhunter.css" />
+</head>
+<body>
+<?php  include 'header.php';?>
+
+<h2 style="text-align:center">Headhunter</h2>
+
+<div class="card">
+  <img src="css/Headhunter_CSS/team1.jpeg" alt="John" style="width:100%">
+  <h1>Sudheera</h1>
+  <p class="title">CEO & Founder</p>
+  <p>Hello, 
+      I'm the founder of CV.LK website.
+  </p>
+  <br>
+  <div>  
+    <div class="rating">
+        <a href="https://www.facebook.com/">
+                <img class="rating" src="css/Headhunter_CSS/facebook_1.png" alt=""/></a> 
+       
+        <a href="https://www.twitter.com/">
+                <img class="rating" src="css/Headhunter_CSS/twitter_1.png" alt=""/></a> 
+
+        <a href="https://www.linkedin.com/">
+                <img class="rating" src="css/Headhunter_CSS/linkedin_1.png" alt=""/></a> 
+       </div>
+  </div>
+  <p><button>Contact - 071 8009 280</button></p>
+</div>
+
+</body>
+<?php include('footer1.php'); ?>
+</html>
