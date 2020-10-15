0016f98c7fe4d1c823cdb784f77a90a14f17dc95
RushdyAhamed <rushdhyahamed98@gmail.com>
2020-10-15 14:47:12 +0530

    collapse with feedbak button

--- jobseeker.php
+++ jobseeker.php
@@ -1,114 +1,105 @@
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
+    <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Job Seekers</title>
         <!-- Bootstrap CSS -->
        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" /> -->
+        <link rel='stylesheet' type='text/css' href='css/bg.css'/>
         <link rel='stylesheet' type='text/css' href='css/bootstrap.min.css'/>
-        <link rel='stylesheet' type='text/css' href='animate.css'/>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel='stylesheet' type='text/css' href='animate.css'/> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="js/jquery-3.5.1.min.js"></script>
+       
+        <script type='text/javascript' src="js/back_2top.js"></script>    
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+        <!-- Additionally added to a collapse -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+        <link rel='stylesheet' type='text/css' href='new.css'/>    
 
 <style>
 
-table.tablee {
-  border-collapse: collapse;
-  border: 1px solid black;
-  width: 75%;
-  margin-left:auto;
-  margin-right:auto;
+/* collapse css */
+
+.bs-example {
+  margin: 30px;
+  width: 25%;
+}
+.accordion .fa{
+  margin-right: 0.5rem;
+}
+.bs-example2{
+  margin: 30px;
+  width: 25%;
+}
+.bs-example3{
+  margin: 30px;
+  width: 25%;
 }
 
-.navbar1 {
-  overflow: hidden;
-  background-color: #333;
-}
+/* Back to top css */
 
-.navbar1 a {
-  float: left;
-  font-size: 16px;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
+.back-to-top {
+    position: fixed;
+    bottom: 25px;
+    right: 25px;
+    display: none;
+    opacity: 0.8;
+    transition: 0.3s;
 }
-
-.dropdown1 {
-  float: left;
-  overflow: hidden;
-}
-
-.dropdown1 .dropbtn1 {
-  font-size: 16px;  
-  border: none;
-  outline: none;
-  color: white;
-  padding: 14px 16px;
-  background-color: inherit;
-  font-family: inherit;
-  margin: 0;
-}
-
-.navbar1 a:hover, .dropdown1:hover .dropbtn1 {
-  background-color: #7e7e80;
-}
-
-.dropdown1-content {
-  display: none;
-  position: absolute;
-  background-color: #f9f9f9;
-  min-width: 160px;
-  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-  z-index: 1;
-}
-
-.dropdown1-content a {
-  float: none;
-  color: black;
-  padding: 12px 16px;
-  text-decoration: none;
-  display: block;
-  text-align: left;
-}
-
-.dropdown1-content a:hover {
-  background-color: #ddd;
-}
-
-.dropdown1:hover .dropdown1-content {
-  display: block;
-}
+.back-to-top:hover {opacity: 1}
 
 .p3 {
-  color: blue;
   font-size: 17px;
+  color: white;
 }
 </style>
+<script>
+    $(document).ready(function(){
+        // Add minus icon for collapse element which is open by default
+        $(".collapse.show").each(function(){
+        	$(this).prev(".card-header").find(".fa").addClass("fa-minus").removeClass("fa-plus");
+        });
+        
+        // Toggle plus minus icon on show hide of collapse element
+        $(".collapse").on('show.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-plus").addClass("fa-minus");
+        }).on('hide.bs.collapse', function(){
+        	$(this).prev(".card-header").find(".fa").removeClass("fa-minus").addClass("fa-plus");
+        });
+    });
+</script>
 </head>
-
-<body style="background-color:;">
 <?php include('header.php'); ?>
-<br>
+<body class="bg-image" style="background-image: url('images/bg-01.jpg');">
+<?php include('feedback-btn.php'); ?>
+
+<div class="frame"></div>
 
 <!--
 <div class="slideshow-container1">
 
-<div class="mySlides1 fade1">
-  <img src="images/rsz_rsz_24.jpg" style="width:100%" height:100%>
-  <div class="text">Caption Text</div>
-</div>
+        <div class="mySlides1 fade1">
+        <img src="images/rsz_rsz_24.jpg" style="width:100%" height:100%>
+        <div class="text">Caption Text</div>
+        </div>
 
-<div class="mySlides1 fade1">
-  <img src="images/5.jpg" style="width:100%">
-  <div class="text">Caption Two</div>
-</div>
+        <div class="mySlides1 fade1">
+        <img src="images/5.jpg" style="width:100%">
+        <div class="text">Caption Two</div>
+        </div>
 
-<div class="mySlides1 fade1">
-  <img src="images/3.jpg" style="width:100%">
-  <div class="text">Caption Three</div>
-</div>
+        <div class="mySlides1 fade1">
+        <img src="images/3.jpg" style="width:100%">
+        <div class="text">Caption Three</div>
+        </div>
 
 </div>
 <br>
@@ -118,8 +109,163 @@ table.tablee {
   <span class="dot"></span> 
   <span class="dot"></span> 
 </div>
+-->
 
+<p class="p3" style="text-align:center;color:white;">We bring you the best Job Opportunities through Employers, Headhunters, Recruitment Agents <br><br>
+Upload Your CV in the preffered position</p>
+<br>
+
+<div class="bs-example" style="top: 52.3%;left: 600px">
+    <div class="accordion" id="accordionExample">
+
+    <div class="card">
+            <div class="card-header" id="headingOne">
+                <h2 class="mb-0"><button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOne"><i class="fa fa-plus"></i> Information Technology</button></h2>
+            </div>
+              <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                  <div class="card-body">
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Developers</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Designers</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Suitable Position</a></ul>
+                  </div>
+              </div>
+    </div>
+        
+
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h2 class="mb-0"><button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo"><i class="fa fa-plus"></i> Human Resources Management</button></h2>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                <div class="card-body">
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Manager & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Executive & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Suitable Position</a></ul>
+                  </div>
+            </div>
+        </div>
+
+
+        <div class="card">
+            <div class="card-header" id="headingThree">
+                <h2 class="mb-0"><button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree"><i class="fa fa-plus"></i> Marketing & Sales</button></h2>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                <div class="card-body">
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Manager & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Executive & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Suitable Position</a></ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<div class="bs-example2" style="position: absolute;top: 119.7%;left: 475px;">
+    <div class="accordion" id="accordionExample2">
+
+    <div class="card">
+            <div class="card-header" id="headingOneex2">
+                <h2 class="mb-0"><button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOneex2"><i class="fa fa-plus"></i> Production Management</button></h2>
+            </div>
+              <div id="collapseOneex2" class="collapse" aria-labelledby="headingOneex2" data-parent="#accordionExample2">
+                  <div class="card-body">
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Manager & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Executive & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Suitable Position</a></ul>
+                  </div>
+              </div>
+    </div>
+        
+
+        <div class="card">
+            <div class="card-header" id="headingTwoex2">
+                <h2 class="mb-0"><button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseTwoex2"><i class="fa fa-plus"></i> Finance & Accounting</button></h2>
+            </div>
+            <div id="collapseTwoex2" class="collapse" aria-labelledby="headingTwoex2" data-parent="#accordionExample2">
+                <div class="card-body">
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Manager & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Accountant</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Suitable Position</a></ul>
+                  </div>
+            </div>
+        </div>
+
+
+        <div class="card">
+            <div class="card-header" id="headingThreeex2">
+                <h2 class="mb-0"><button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThreeex2"><i class="fa fa-plus"></i> Operation Management</button></h2>
+            </div>
+            <div id="collapseThreeex2" class="collapse" aria-labelledby="headingThreeex2" data-parent="#accordionExample2">
+                <div class="card-body">
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Manager & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Executive & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Suitable Position</a></ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+<div class="bs-example3" style="position: absolute;top: 119.7%;left: 950px;">
+    <div class="accordion" id="accordionExample3">
+
+    <div class="card">
+            <div class="card-header" id="headingOneex3">
+                <h2 class="mb-0"><button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseOneex3"><i class="fa fa-plus"></i> Engineering</button></h2>
+            </div>
+              <div id="collapseOneex3" class="collapse" aria-labelledby="headingOneex3" data-parent="#accordionExample3">
+                  <div class="card-body">
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Civil Engineering</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Electrical Engineering</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Mechanical Engineering</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Suitable Position</a></ul>
+                  </div>
+              </div>
+    </div>
+        
+
+        <div class="card">
+            <div class="card-header" id="headingTwoex3">
+                <h2 class="mb-0"><button type="button" class="btn btn-link" data-toggle="collapse" data-target="#collapseTwoex3"><i class="fa fa-plus"></i> Maintenance Management</button></h2>
+            </div>
+            <div id="collapseTwoex3" class="collapse" aria-labelledby="headingTwoex3" data-parent="#accordionExample3">
+                <div class="card-body">
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Manager & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Executive & Above</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Suitable Position</a></ul>
+                  </div>
+            </div>
+        </div>
+
+
+        <div class="card">
+            <div class="card-header" id="headingThreeex3">
+                <h2 class="mb-0"><button type="button" class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThreeex3"><i class="fa fa-plus"></i> Lecturers</button></h2>
+            </div>
+            <div id="collapseThreeex3" class="collapse" aria-labelledby="headingThreeex3" data-parent="#accordionExample3">
+                <div class="card-body">
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Full Time</a></ul>
+                    <ul>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="apply_form.php">Part Time</a></ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<br><br>
+
+
+<!--
 <script>
+
+/* Function for animation */
+
 var slideIndex = 0;
 showSlides();
 
@@ -140,146 +286,13 @@ function showSlides() {
   setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
-
 </script>
-
 -->
 
-<br>
-<p class="p3" style="text-align:center">We bring you the best Job Opportunities through Employers, Headhunters, Recruitment Agents <br><br>
-Upload Your CV in the preffered position</p>
-<br>
-
-<table class="tablee">
-  <tr>
-    <td><div class="navbar1">
-  <div class="dropdown1">
-    <button class="dropbtn1">Human Resources Management 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown1-content">
-      <a href="#">Manager & Above</a>
-      <a href="#">Executive & Above</a>
-      <a href="#">Suitable Post</a>
-    </div>
-  </div></div> </td>
-
-  <td><div class="navbar1">
-        <div class="dropdown1">
-    <button class="dropbtn1">Logistics & Supply chain management
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown1-content">
-      <a href="#">Manager & Above</a>
-      <a href="#">Executive & Above</a>
-      <a href="#">Suitable Post</a>
-    </div>
-  </div></div></td>
-
-    
-
-  <td><div class="navbar1">
-        <div class="dropdown1">
-    <button class="dropbtn1">Production Management 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown1-content">
-      <a href="#">Manager & Above</a>
-      <a href="#">Executive & Above</a>
-      <a href="#">Suitable Post</a>
-    </div>
-  </div></div></td>
-  </tr>
-
-  <tr>
-    <td>
-    <div class="navbar1">
-    <div class="dropdown1">
-    <button class="dropbtn1">Maintenance Management 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown1-content">
-      <a href="#">Manager & Above</a>
-      <a href="#">Executive & Above</a>
-      <a href="#">Suitable Post</a>
-    </div>
-  </div></div>
-</td>
-
-<td><div class="navbar1">
-        <div class="dropdown1">
-    <button class="dropbtn1">Information Technology 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown1-content">
-      <a href="#">Developers</a>
-      <a href="#">Designers</a>
-      <a href="#">Suitable Position</a>
-    </div>
-  </div></div></td>
-
-  <td><div class="navbar1">
-        <div class="dropdown1">
-    <button class="dropbtn1">Engineering
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown1-content">
-      <a href="#">Civil Engineering</a>
-      <a href="#">Electrical Engineering</a>
-      <a href="#">Mechanical Engineering</a>
-      <a href="#">Suitable Post</a>
-    </div>
-  </div></div></td>
-
-
-  </tr>
-
-
-  <tr>
-    <td><div class="navbar1">
-        <div class="dropdown1">
-    <button class="dropbtn1">Finance & Accounting 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown1-content">
-      <a href="#">Manager & Above</a>
-      <a href="#">Executive & Above</a>
-      <a href="#">Suitable Post</a>
-    </div>
-  </div></div>
-</td>
-    
-<td><div class="navbar1">
-        <div class="dropdown1">
-    <button class="dropbtn1">Marketing & Sales 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown1-content">
-      <a href="#">Manager & Above</a>
-      <a href="#">Executive & Above</a>
-      <a href="#">Suitable Post</a>
-    </div>
-  </div></div></div>
-</td>
-
-<td><div class="navbar1">
-        <div class="dropdown1">
-    <button class="dropbtn1">Lecturers 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown1-content">
-      <a href="#">Full Time</a>
-      <a href="#">Part Time</a>
-    </div>
-  </div></div></div>
-</td>
-  
-
-  </tr>
-</table>
-<br><br><br>
+<br><br><br><br><br><br>
    
-
+<!--back to top -->
+<a id="back-to-top" href="#" class="btn btn-dark btn-lg back-to-top" style='background-color:#00FEDE' role="button"><i class="fas fa-chevron-up" style="color:black"></i></a>
   
 <?php include('footer1.php'); ?>
 </body>
